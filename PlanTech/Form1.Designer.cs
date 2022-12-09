44b44096ba9aaf453f6a3e8c9ec395a673ddb267
Uri2099 <p20s070044@itsatlixco.edu.mx>
2022-12-08 21:30:07 -0600

    Calculadora

--- PlanTech/Form1.Designer.cs
+++ PlanTech/Form1.Designer.cs
@@ -33,14 +33,26 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.calculadora = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.num1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.calculadora.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.calculadora);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(2, 1);
@@ -56,18 +68,18 @@
             "B",
             "C",
             "D"});
-            this.comboBox1.Location = new System.Drawing.Point(939, 74);
+            this.comboBox1.Location = new System.Drawing.Point(37, 241);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(308, 21);
+            this.comboBox1.Size = new System.Drawing.Size(181, 21);
             this.comboBox1.TabIndex = 3;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(349, 273);
+            this.button1.Location = new System.Drawing.Point(194, 64);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(208, 89);
+            this.button1.Size = new System.Drawing.Size(97, 33);
             this.button1.TabIndex = 2;
             this.button1.Text = "Si";
             this.button1.UseVisualStyleBackColor = true;
@@ -75,7 +87,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::PlanTech.Properties.Resources._200w;
-            this.pictureBox1.Location = new System.Drawing.Point(722, 371);
+            this.pictureBox1.Location = new System.Drawing.Point(570, 388);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(208, 162);
             this.pictureBox1.TabIndex = 1;
@@ -92,6 +104,123 @@
             this.label1.Text = "Viva Amlo, Asuka\r\n y el niño Jesus";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // calculadora
+            // 
+            this.calculadora.Controls.Add(this.button10);
+            this.calculadora.Controls.Add(this.button1);
+            this.calculadora.Controls.Add(this.comboBox1);
+            this.calculadora.Controls.Add(this.button9);
+            this.calculadora.Controls.Add(this.button8);
+            this.calculadora.Controls.Add(this.button7);
+            this.calculadora.Controls.Add(this.button6);
+            this.calculadora.Controls.Add(this.button5);
+            this.calculadora.Controls.Add(this.button4);
+            this.calculadora.Controls.Add(this.button3);
+            this.calculadora.Controls.Add(this.button2);
+            this.calculadora.Controls.Add(this.num1);
+            this.calculadora.Controls.Add(this.textBox1);
+            this.calculadora.Location = new System.Drawing.Point(918, 303);
+            this.calculadora.Name = "calculadora";
+            this.calculadora.Size = new System.Drawing.Size(344, 322);
+            this.calculadora.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(37, 64);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(28, 23);
+            this.num1.TabIndex = 1;
+            this.num1.Text = "1";
+            this.num1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(105, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(28, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "9";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(71, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(28, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "8";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(37, 122);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "7";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(105, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(28, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "6";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(71, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(28, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "5";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(37, 93);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(28, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "4";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(105, 64);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(28, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "3";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(71, 64);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(28, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "2";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(37, 151);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(28, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -103,6 +232,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.calculadora.ResumeLayout(false);
+            this.calculadora.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,6 +245,18 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel calculadora;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button num1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button10;
     }
 }
 
