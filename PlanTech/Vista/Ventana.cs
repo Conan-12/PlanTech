9804d46a213e0669fa633028ac3251f23fc82f31
Conan-12 <p20s070046@itsatlixco.edu.mx>
2022-12-09 22:40:46 -0600

    Nada

---
+++ PlanTech/Vista/Ventana.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanTech.Vista
+{
+    public partial class Ventana : UserControl
+    {
+        public Ventana()
+        {
+            InitializeComponent();
+        }
+    }
+}
